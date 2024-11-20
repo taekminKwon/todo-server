e44c13a00b73b137539ac17b4a06083a28696e05
TaekMin <rnjsxorals1@gmail.com>
2024-11-20 15:37:27 +0900

    [#2] 할일 조회 API 기능 구현

--- src/main/java/org/example/service/TaskService.java
+++ src/main/java/org/example/service/TaskService.java
@@ -10,6 +10,8 @@ import org.example.persist.entity.TaskEntity;
 
 import java.sql.Date;
 import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -28,6 +30,34 @@ public class TaskService {
         return entityToObject(saved);
     }
 
+    public List<Task> getALL(){
+        return this.taskRepository.findAll().stream()
+                .map(this::entityToObject)
+                .collect(Collectors.toList());
+    }
+
+    public List<Task> getByDueDate(String DueDate) {
+        return this.taskRepository.findAllByDueDate(Date.valueOf(DueDate)).stream()
+                .map(this::entityToObject)
+                .collect(Collectors.toList());
+    }
+
+    public List<Task> getByStatus(TaskStatus status){
+        return this.taskRepository.findAllByStatus(status).stream()
+                .map(this::entityToObject)
+                .collect(Collectors.toList());
+    }
+
+    public Task getOne(Long id){
+        var entity = this.getById(id);
+        return this.entityToObject(entity);
+    }
+
+    private TaskEntity getById(Long id){
+        return this.taskRepository.findById(id)
+                .orElseThrow(() ->
+                        new IllegalArgumentException(String.format("not exists task id [%d]",id)));
+    }
     private Task entityToObject(TaskEntity e){
         return Task.builder()
                 .id(e.getId())
--- src/main/java/org/example/web/TaskController.java
+++ src/main/java/org/example/web/TaskController.java
@@ -2,14 +2,16 @@ package org.example.web;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.example.constants.TaskStatus;
 import org.example.model.Task;
 import org.example.web.vo.TaskRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.example.service.TaskService;
+
+import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @Controller
@@ -29,4 +31,29 @@ public class TaskController {
         this.taskService.add(req.getTitle(), req.getDescription(), req.getDueDate());
         return null;
     }
+
+    @GetMapping
+    public ResponseEntity<List<Task>> getTask(Optional<String> dueDate){
+        List<Task> result;
+
+        if(dueDate.isPresent()){
+            result = this.taskService.getByDueDate(dueDate.get());
+        } else {
+            result =this.taskService.getALL();
+        }
+        return ResponseEntity.ok(result);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Task> fetchOneTask(@PathVariable Long id){
+        var result = this.taskService.getOne(id);
+        return ResponseEntity.ok(result);
+    }
+
+    @GetMapping("/status/{status}")
+    public ResponseEntity<List<Task>> getByStatus(@PathVariable TaskStatus status){
+        var result = this.taskService.getByStatus(status);
+        return ResponseEntity.ok(result);
+    }
+
 }
